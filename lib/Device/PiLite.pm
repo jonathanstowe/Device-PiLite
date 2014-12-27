05b86a46b117be84d3b407c8139afb5940318a5a
Jonathan Stowe <jns@gellyfish.co.uk>
2014-12-27 22:28:51 +0000

    Initial commit

---
+++ lib/Device/PiLite.pm
@@ -0,0 +1,95 @@
+package Device::PiLite;
+
+use strict;
+use warnings;
+
+use Moose;
+
+
+=head1 NAME
+
+Device::PiLite - Interface to Ciseco Pi-Lite for Raspberry Pi
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+
+
+    use Device::PiLite;
+
+    my $pilite = Device::PiLite->new();
+    ...
+
+
+
+
+=head2 DESCRIPTION
+
+=head2 METHODS
+
+=cut
+
+=over 4
+
+=back
+
+=head1 AUTHOR
+
+Jonathan Stowe, C<< <jns at gellyfish.co.uk> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-device-pilite at rt.cpan.org>
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Device::PiLite
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Device-PiLite>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Device-PiLite>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Device-PiLite>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Device-PiLite/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2014 Jonathan Stowe.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See L<http://dev.perl.org/licenses/> for more information.
+
+
+=cut
+
+1; 
